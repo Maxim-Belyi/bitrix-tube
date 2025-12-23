ee63eb980b69922c83b2a0eb10c422abdf32a4db
Maxim Belyi <btf9458@gmail.com>
2025-12-23 18:57:11 +0500

    Добавление инфоблока 'разработка'

--- local/templates/landing24/components/bitrix/news.list/services_cards/template.php
+++ local/templates/landing24/components/bitrix/news.list/services_cards/template.php
@@ -1,4 +1,5 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+	die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -10,77 +11,44 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
+
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
-		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
-			<?else:?>
-				<b><?echo $arItem["NAME"]?></b><br />
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;?>
-	</p>
-<?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
+
+<div class="row">
+	<?php foreach ($arResult["ITEMS"] as $arItem): ?>
+		<?php
+		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+		$this->AddDeleteAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"] . "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+		?>
+		
+
+		<?php if (!empty($arItem["NAME"])): ?>
+		
+			<div class="col-lg-3 col-md-3 col-sm-6" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+				<div class="single-pricing text-center mb-30">
+					<div class="pricing-head">
+						<h2 class="pricing-title text-uppercase"><?= $arItem["NAME"] ?></h2>
+						<?php if (!empty($arItem["DETAIL_TEXT"])): ?>
+							<span><?= $arItem["DETAIL_TEXT"] ?></span>
+						<?php endif; ?>
+					</div>
+					<div class="pricing-plan-price <?=$arItem["PROPERTIES"]["BG_PRICE_BUTTON"]["VALUE_XML_ID"] ?>">
+						<?= $arItem["PREVIEW_TEXT"] ?>
+					</div>
+
+					<div class="pricing-plan-list">
+						<ul>
+							<?php foreach ($arItem["PROPERTIES"]["DEV_LI_ITEMS"]["VALUE"] as $value): ?>
+								<li><?=$value ?></li>
+							<?php endforeach ?>
+						</ul>
+					</div>
+					<div class="get-started">
+						<a href="#" class="btn <?=$arItem["PROPERTIES"]["BG_PRICE_BUTTON"]["VALUE_XML_ID"]?>">Узнать больше</a>
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
+	<?php endforeach; ?>
 </div>
--- services/index.php
+++ services/index.php
@@ -232,169 +232,75 @@ $APPLICATION->SetPageProperty("services", "Чем мы занимаемся");
             </div>
         </div>
 
-        <?$APPLICATION->IncludeComponent("bitrix:news.list", "services_cards", Array(
-	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
-		"ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
-		"AJAX_MODE" => "N",	// Включить режим AJAX
-		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
-		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-		"AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
-		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
-		"CACHE_GROUPS" => "N",	// Учитывать права доступа
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
-		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
-		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
-		"DISPLAY_DATE" => "N",	// Выводить дату элемента
-		"DISPLAY_NAME" => "Y",	// Выводить название элемента
-		"DISPLAY_PICTURE" => "N",	// Выводить изображение для анонса
-		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
-		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
-		"FIELD_CODE" => array(	// Поля
-			0 => "NAME",
-			1 => "PREVIEW_TEXT",
-			2 => "DETAIL_TEXT",
-			3 => "DETAIL_PICTURE",
-			4 => "",
-		),
-		"FILTER_NAME" => "",	// Фильтр
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
-		"IBLOCK_ID" => "10",	// Код информационного блока
-		"IBLOCK_TYPE" => "Content",	// Тип информационного блока (используется только для проверки)
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
-		"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
-		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
-		"NEWS_COUNT" => "20",	// Количество новостей на странице
-		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
-		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
-		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
-		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
-		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
-		"PAGER_TITLE" => "Новости",	// Название категорий
-		"PARENT_SECTION" => "",	// ID раздела
-		"PARENT_SECTION_CODE" => "",	// Код раздела
-		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
-		"PROPERTY_CODE" => array(	// Свойства
-			0 => "DEV_LI_ITEMS",
-			1 => "BG_PRICE_BUTTON",
-			2 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
-		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
-		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
-		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
-		"SET_STATUS_404" => "N",	// Устанавливать статус 404
-		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
-		"SHOW_404" => "N",	// Показ специальной страницы
-		"SORT_BY1" => "ID",	// Поле для первой сортировки новостей
-		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
-		"SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
-		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
-		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
-	),
-	false
-);?>
-        <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="single-pricing text-center mb-30">
-                    <div class="pricing-head">
-                        <h2 class="pricing-title text-uppercase">Лендинг</h2>
-                        <span>Одностраничный сайт</span>
-                    </div>
-                    <div class="pricing-plan-price basic-bg">
-                        <span><span>от </span>10 000<span> ₽</span></span>
-                    </div>
-                    <div class="pricing-plan-list">
-                        <ul>
-                            <li>до 2х недель</li>
-                            <li>Индивидуальный дизайн</li>
-                            <li>Домен + хостинг на 1 год</li>
-                            <li>-</li>
-                            <li>-</li>
-                            <li>-</li>
-                        </ul>
-                    </div>
-                    <div class="get-started">
-                        <a href="../../../../bitrix/urlrewrite.php" class="btn">Узнать больше</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="single-pricing text-center mb-30">
-                    <div class="pricing-head">
-                        <h2 class="pricing-title text-uppercase">Сайт визитка</h2>
-                        <span></span>
-                    </div>
-                    <div class="pricing-plan-price standard-bg">
-                        <span><span>от </span>15 000<span> ₽</span></span>
-                    </div>
-                    <div class="pricing-plan-list">
-                        <ul>
-                            <li>от 1 до 2 недель</li>
-                            <li>Индивидуальный дизайн</li>
-                            <li>Домен + хостинг на 1 год</li>
-                            <li>Удобная админка</li>
-                            <li>-</li>
-                            <li>-</li>
-                        </ul>
-                    </div>
-                    <div class="get-started">
-                        <a href="#" class="btn standard-bg">Узнать больше</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="single-pricing text-center mb-30">
-                    <div class="pricing-head">
-                        <h2 class="pricing-title text-uppercase">Интернет магазин</h2>
-                        <span></span>
-                    </div>
-                    <div class="pricing-plan-price premium-bg">
-                        <span><span>от </span>30 000<span> ₽</span></span>
-                    </div>
-                    <div class="pricing-plan-list">
-                        <ul>
-                            <li>от 3х недель до 1,5 мес.</li>
-                            <li>Индивидуальный дизайн</li>
-                            <li>Домен + хостинг на 1 год</li>
-                            <li>Удобная админка</li>
-                            <li>Интеграции с внешними сервисами</li>
-                            <li>Базовая SEO-оптимизация</li>
-                        </ul>
-                    </div>
-                    <div class="get-started">
-                        <a href="../../../../bitrix/urlrewrite.php" class="btn premium-bg">Узнать больше</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="single-pricing text-center mb-30">
-                    <div class="pricing-head">
-                        <h2 class="pricing-title text-uppercase">Корп. сайт</h2>
-                        <span>Любой сложности</span>
-                    </div>
-                    <div class="pricing-plan-price unlimited-bg">
-                        <span><span>от </span>50 000<span> ₽</span></span>
-                    </div>
-                    <div class="pricing-plan-list">
-                        <ul>
-                            <li>до 2 мес.</li>
-                            <li>Индивидуальный дизайн</li>
-                            <li>Домен + хостинг на 1 год</li>
-                            <li>Удобная панель управлени</li>
-                            <li>Интеграции с внешними сервисами</li>
-                            <li>-</li>
-                        </ul>
-                    </div>
-                    <div class="get-started">
-                        <a href="#" class="btn unlimited-bg">Узнать больше</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news.list",
+            "services_cards",
+            array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_SECTIONS_CHAIN" => "Y",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "N",
+                "CACHE_FILTER" => "N",
+                "CACHE_GROUPS" => "N",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "Y",
+                "DISPLAY_DATE" => "N",
+                "DISPLAY_NAME" => "Y",
+                "DISPLAY_PICTURE" => "N",
+                "DISPLAY_PREVIEW_TEXT" => "Y",
+                "DISPLAY_TOP_PAGER" => "N",
+                "FIELD_CODE" => array(
+                    0 => "NAME",
+                    1 => "PREVIEW_TEXT",
+                    2 => "DETAIL_TEXT",
+                    3 => "",
+                ),
+                "FILTER_NAME" => "",
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                "IBLOCK_ID" => "10",
+                "IBLOCK_TYPE" => "Content",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "INCLUDE_SUBSECTIONS" => "N",
+                "MESSAGE_404" => "",
+                "NEWS_COUNT" => "20",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_DESC_NUMBERING" => "N",
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_SHOW_ALWAYS" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Новости",
+                "PARENT_SECTION" => "",
+                "PARENT_SECTION_CODE" => "",
+                "PREVIEW_TRUNCATE_LEN" => "",
+                "PROPERTY_CODE" => array(
+                    0 => "DEV_LI_ITEMS",
+                    1 => "BG_PRICE_BUTTON",
+                    2 => "",
+                ),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "SORT_BY1" => "ID",
+                "SORT_BY2" => "SORT",
+                "SORT_ORDER1" => "ASC",
+                "SORT_ORDER2" => "ASC",
+                "STRICT_SECTION_CHECK" => "N",
+                "COMPONENT_TEMPLATE" => "services_cards"
+            ),
+            false
+        ); ?>
+
     </div>
 </section>
 
