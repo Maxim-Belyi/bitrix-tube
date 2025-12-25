e0149c7469e918739f9f6fa0d56c276f7b4496ee
Maxim Belyi <btf9458@gmail.com>
2025-12-25 08:30:05 +0500

    Создание комплексного компонента news

--- portfolio/index.php
+++ portfolio/index.php
@@ -3,167 +3,136 @@ global $APPLICATION;
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Наши работы");
 
-$APPLICATION->SetPageProperty("title", "Портфолио");
+$APPLICATION->SetPageProperty("title", "Портфолио | We coders");
 ?>
 <section class="work-area pad-90">
     <div class="container">
         <div class="row">
             <div class="section-heading text-center mb-70">
                 <h2>
-                    <?php $APPLICATION->IncludeComponent(
+                    <? $APPLICATION->IncludeComponent(
                         "bitrix:main.include",
                         "",
-                        [
+                        array(
                             "AREA_FILE_SHOW" => "file",
-                            "PATH" => SITE_TEMPLATE_PATH . "/includes/portfolio/title.php",
-                        ]
-                    ) ?>
-                    </h2>
-                <p><?php $APPLICATION->IncludeComponent(
+                            "PATH" => SITE_TEMPLATE_PATH . "/includes/portfolio/title.php"
+                        )
+                    ); ?>
+                </h2>
+                <p>
+                    <? $APPLICATION->IncludeComponent(
                         "bitrix:main.include",
                         "",
-                        [
+                        array(
                             "AREA_FILE_SHOW" => "file",
-                            "PATH" => SITE_TEMPLATE_PATH . "/includes/portfolio/title.php",
-                        ]
-                    ) ?></p>
+                            "PATH" => SITE_TEMPLATE_PATH . "/includes/portfolio/title.php"
+                        )
+                    ); ?>
+                </p>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="portfolio-menu brand-filter text-center mb-70">
-                    <div class="filter" data-filter="all">Все</div>
-                    <div class="filter" data-filter=".landing">Лендинги</div>
-                    <div class="filter" data-filter=".internet_shop">Интренет магазины</div>
-                    <div class="filter" data-filter=".promo">Промо сайты</div>
-                    <div class="filter" data-filter=".corporative_site">Корпоративные порталы</div>
-                </div>
-            </div>
-
-            <div id="Container">
-                <div class="col-md-4 col-sm-6 col-xs-12 mb-30 mix landing promo">
-                    <div class="portfolio-wrapper portfolio-title">
-                        <div class="portfolio-img">
-                            <img src="img/portfolio/1.jpg" alt=""/>
-                            <div class="work-text brand-bg">
-                                <div class="inner-text">
-                                    <a class="view-portfolio image-link" href="img/portfolio/1.jpg">
-                                        <span class="plus"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-heading pd-15">
-                            <h4 class="mb-10">
-                                <a href="#">Green Planet</a>
-                            </h4>
-                            <h5 class="m-0">Дизайн</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 mb-30 mix internet_shop corporative_site">
-                    <div class="portfolio-wrapper portfolio-title">
-                        <div class="portfolio-img">
-                            <img src="img/portfolio/2.jpg" alt=""/>
-                            <div class="work-text brand-bg">
-                                <div class="inner-text">
-                                    <a class="view-portfolio image-link" href="img/portfolio/2.jpg">
-                                        <span class="plus"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-heading pd-15">
-                            <h4 class="mb-10">
-                                <a href="#">Creative developers</a>
-                            </h4>
-                            <h5 class="m-0">Разработка лендинга</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 mb-30 mix landing">
-                    <div class="portfolio-wrapper portfolio-title">
-                        <div class="portfolio-img">
-                            <img src="img/portfolio/3.jpg" alt=""/>
-                            <div class="work-text brand-bg">
-                                <div class="inner-text">
-                                    <a class="view-portfolio image-link" href="img/portfolio/3.jpg"><span class="plus"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-heading pd-15">
-                            <h4 class="mb-10">
-                                <a href="../../../../bitrix/urlrewrite.php">Dress & Jeans</a>
-                            </h4>
-                            <h5 class="m-0">Разработка интернет-магазина</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 mb-30 mix internet_shop">
-                    <div class="portfolio-wrapper portfolio-title">
-                        <div class="portfolio-img">
-                            <img src="img/portfolio/4.jpg" alt=""/>
-                            <div class="work-text brand-bg">
-                                <div class="inner-text">
-                                    <a class="view-portfolio image-link" href="img/portfolio/4.jpg"><span
-                                            class="plus"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-heading pd-15">
-                            <h4 class="mb-10">
-                                <a href="../../../../bitrix/urlrewrite.php">Mountain King</a>
-                            </h4>
-                            <h5 class="m-0">Разработка сайта визитки</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 mb-30 mix corporative_site">
-                    <div class="portfolio-wrapper portfolio-title">
-                        <div class="portfolio-img">
-                            <img src="img/portfolio/5.jpg" alt=""/>
-                            <div class="work-text brand-bg">
-                                <div class="inner-text">
-                                    <a class="view-portfolio image-link" href="img/portfolio/5.jpg">
-                                        <span class="plus"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-heading pd-15">
-                            <h4 class="mb-10">
-                                <a href="#">Beauty SPA</a>
-                            </h4>
-                            <h5 class="m-0">Разработка сайта визитки</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 mb-30 mix promo">
-                    <div class="portfolio-wrapper portfolio-title">
-                        <div class="portfolio-img">
-                            <img src="img/portfolio/6.jpg" alt=""/>
-                            <div class="work-text brand-bg">
-                                <div class="inner-text">
-                                    <a class="view-portfolio image-link" href="img/portfolio/6.jpg"><span class="plus"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-heading pd-15">
-                            <h4 class="mb-10">
-                                <a href="#">Bent Application</a>
-                            </h4>
-                            <h5 class="m-0">Разработка лендинга</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
+        <br>
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news",
+            "portfolio_news",
+            array(
+                "ADD_ELEMENT_CHAIN" => "N",	// Включать название элемента в цепочку навигации
+                "ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+                "AJAX_MODE" => "N",	// Включить режим AJAX
+                "AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+                "AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+                "AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+                "AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
+                "BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
+                "CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+                "CACHE_GROUPS" => "N",	// Учитывать права доступа
+                "CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+                "CACHE_TYPE" => "A",	// Тип кеширования
+                "CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+                "DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+                "DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+                "DETAIL_DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+                "DETAIL_FIELD_CODE" => array(	// Поля
+                    0 => "CODE",
+                    1 => "NAME",
+                    2 => "PREVIEW_TEXT",
+                    3 => "PREVIEW_PICTURE",
+                    4 => "DETAIL_TEXT",
+                    5 => "DETAIL_PICTURE",
+                    6 => "",
+                ),
+                "DETAIL_PAGER_SHOW_ALL" => "Y",	// Показывать ссылку "Все"
+                "DETAIL_PAGER_TEMPLATE" => "",	// Название шаблона
+                "DETAIL_PAGER_TITLE" => "Страница",	// Название категорий
+                "DETAIL_PROPERTY_CODE" => array(	// Свойства
+                    0 => "additional_info",
+                    1 => "detail_title",
+                    2 => "detail_description",
+                    3 => "link",
+                    4 => "gallery",
+                    5 => "",
+                ),
+                "DETAIL_SET_CANONICAL_URL" => "N",	// Устанавливать канонический URL
+                "DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+                "DISPLAY_DATE" => "N",	// Выводить дату элемента
+                "DISPLAY_NAME" => "Y",	// Выводить название элемента
+                "DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+                "DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+                "DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+                "IBLOCK_ID" => "15",	// Инфоблок
+                "IBLOCK_TYPE" => "Portfolio",	// Тип инфоблока
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+                "LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+                "LIST_FIELD_CODE" => array(	// Поля
+                    0 => "CODE",
+                    1 => "NAME",
+                    2 => "PREVIEW_TEXT",
+                    3 => "PREVIEW_PICTURE",
+                    4 => "DETAIL_TEXT",
+                    5 => "DETAIL_PICTURE",
+                    6 => "",
+                ),
+                "LIST_PROPERTY_CODE" => array(	// Свойства
+                    0 => "",
+                    1 => "",
+                ),
+                "MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+                "META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
+                "META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
+                "NEWS_COUNT" => "20",	// Количество новостей на странице
+                "PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+                "PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+                "PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+                "PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+                "PAGER_TEMPLATE" => "grid",	// Шаблон постраничной навигации
+                "PAGER_TITLE" => "Новости",	// Название категорий
+                "PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+                "SEF_MODE" => "N",	// Включить поддержку ЧПУ
+                "SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+                "SET_STATUS_404" => "N",	// Устанавливать статус 404
+                "SET_TITLE" => "N",	// Устанавливать заголовок страницы
+                "SHOW_404" => "N",	// Показ специальной страницы
+                "SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
+                "SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+                "SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
+                "SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+                "STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела
+                "USE_CATEGORIES" => "N",	// Выводить материалы по теме
+                "USE_FILTER" => "N",	// Показывать фильтр
+                "USE_PERMISSIONS" => "N",	// Использовать дополнительное ограничение доступа
+                "USE_RATING" => "N",	// Разрешить голосование
+                "USE_RSS" => "N",	// Разрешить RSS
+                "USE_SEARCH" => "N",	// Разрешить поиск
+                "USE_SHARE" => "N",	// Отображать панель соц. закладок
+                "VARIABLE_ALIASES" => array(
+                    "ELEMENT_ID" => "ELEMENT_ID",
+                    "SECTION_ID" => "SECTION_ID",
+                )
+            ),
+            false
+        ); ?>
     </div>
 </section>
 <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
