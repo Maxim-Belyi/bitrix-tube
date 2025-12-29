d0a9d488aecd5948d194bdc00787f2eeabb592d4
Maxim Belyi <btf9458@gmail.com>
2025-12-29 18:29:00 +0500

    Добавлен слайдер на страницу о нас

--- about/index.php
+++ about/index.php
@@ -57,132 +57,153 @@ $APPLICATION->SetPageProperty("description", "О нашей компании");
                             )
                         ); ?>
                     </h2>
-                    <p>  <? $APPLICATION->IncludeComponent(
-                            "bitrix:main.include",
-                            "",
-                            array(
-                                "AREA_FILE_SHOW" => "file",
-                                "PATH" => SITE_TEMPLATE_PATH . "/includes/about/about_team_descr.php"
-                            )
-                        ); ?></p>
+                    <p> <? $APPLICATION->IncludeComponent(
+                        "bitrix:main.include",
+                        "",
+                        array(
+                            "AREA_FILE_SHOW" => "file",
+                            "PATH" => SITE_TEMPLATE_PATH . "/includes/about/about_team_descr.php"
+                        )
+                    ); ?></p>
                 </div>
             </div>
 
-            <?$APPLICATION->IncludeComponent("bitrix:news.list", "about_team", Array(
-	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
-		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
-		"AJAX_MODE" => "N",	// Включить режим AJAX
-		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
-		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-		"AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
-		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
-		"CACHE_GROUPS" => "N",	// Учитывать права доступа
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CHECK_DATES" => "N",	// Показывать только активные на данный момент элементы
-		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
-		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
-		"DISPLAY_DATE" => "N",	// Выводить дату элемента
-		"DISPLAY_NAME" => "Y",	// Выводить название элемента
-		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
-		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
-		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
-		"FIELD_CODE" => array(	// Поля
-			0 => "NAME",
-			1 => "PREVIEW_TEXT",
-			2 => "PREVIEW_PICTURE",
-			3 => "DETAIL_TEXT",
-			4 => "",
-		),
-		"FILTER_NAME" => "",	// Фильтр
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
-		"IBLOCK_ID" => "19",	// Код информационного блока
-		"IBLOCK_TYPE" => "about",	// Тип информационного блока (используется только для проверки)
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
-		"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
-		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
-		"NEWS_COUNT" => "20",	// Количество новостей на странице
-		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
-		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
-		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
-		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
-		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
-		"PAGER_TITLE" => "Новости",	// Название категорий
-		"PARENT_SECTION" => "",	// ID раздела
-		"PARENT_SECTION_CODE" => "",	// Код раздела
-		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
-		"PROPERTY_CODE" => array(	// Свойства
-			0 => "social_links",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
-		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
-		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
-		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
-		"SET_STATUS_404" => "N",	// Устанавливать статус 404
-		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
-		"SHOW_404" => "N",	// Показ специальной страницы
-		"SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
-		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
-		"SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
-		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
-		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
-	),
-	false
-);?>
-            
+            <? $APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "about_team",
+                array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+                    "ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+                    "AJAX_MODE" => "N",	// Включить режим AJAX
+                    "AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+                    "AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+                    "AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+                    "AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
+                    "CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+                    "CACHE_GROUPS" => "N",	// Учитывать права доступа
+                    "CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+                    "CACHE_TYPE" => "A",	// Тип кеширования
+                    "CHECK_DATES" => "N",	// Показывать только активные на данный момент элементы
+                    "DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+                    "DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+                    "DISPLAY_DATE" => "N",	// Выводить дату элемента
+                    "DISPLAY_NAME" => "Y",	// Выводить название элемента
+                    "DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+                    "DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+                    "DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+                    "FIELD_CODE" => array(	// Поля
+                        0 => "NAME",
+                        1 => "PREVIEW_TEXT",
+                        2 => "PREVIEW_PICTURE",
+                        3 => "DETAIL_TEXT",
+                        4 => "",
+                    ),
+                    "FILTER_NAME" => "",	// Фильтр
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+                    "IBLOCK_ID" => "19",	// Код информационного блока
+                    "IBLOCK_TYPE" => "about",	// Тип информационного блока (используется только для проверки)
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+                    "INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
+                    "MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+                    "NEWS_COUNT" => "20",	// Количество новостей на странице
+                    "PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+                    "PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+                    "PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+                    "PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+                    "PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+                    "PAGER_TITLE" => "Новости",	// Название категорий
+                    "PARENT_SECTION" => "",	// ID раздела
+                    "PARENT_SECTION_CODE" => "",	// Код раздела
+                    "PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+                    "PROPERTY_CODE" => array(	// Свойства
+                        0 => "social_links",
+                        1 => "",
+                    ),
+                    "SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+                    "SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+                    "SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+                    "SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
+                    "SET_STATUS_404" => "N",	// Устанавливать статус 404
+                    "SET_TITLE" => "N",	// Устанавливать заголовок страницы
+                    "SHOW_404" => "N",	// Показ специальной страницы
+                    "SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+                    "SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+                    "SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
+                    "SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+                    "STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+                ),
+                false
+            ); ?>
+
         </div>
     </div>
 
-    <!-- Отзывы -->
-    <!-- <section class="testimonial-area pad-90">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="testimonial-active dots-style">
-                        <div class="single-testimonial black-text text-center">
-                            <div class="testimonial-title">
-                                <span class="icon-quote"></span>
-                                <h3 class="black-text">Сроки не проблема!</h3>
-                            </div>
-                            <p><span>"</span>Спасут любой проект с самыми горящими сроками! Ребята действительно
-                                профессионалы своего дела.<span>"</span>
-                            </p>
-                            <div class="testimonial-author text-uppercase">
-                                <span>- Андрей Александров, Best Shop.</span>
-                            </div>
-                        </div>
-                        <div class="single-testimonial black-text text-center">
-                            <div class="testimonial-title">
-                                <span class="icon-quote"></span>
-                                <h3 class="black-text">Это взрыв!</h3>
-                            </div>
-                            <p><span>"</span>Я рад, что мы получили такой классный продукт на выходе!
-                                Думаю, что совместными усилиями мы реализуем ещё не один
-                                проект.<span>"</span></p>
-                            <div class="testimonial-author text-uppercase">
-                                <span>- Виталий Нохрин, NA'MAN Rec.</span>
-                            </div>
-                        </div>
-                        <div class="single-testimonial black-text text-center">
-                            <div class="testimonial-title">
-                                <span class="icon-quote"></span>
-                                <h3 class="black-text">Большая удача?</h3>
-                            </div>
-                            <p><span>"</span>Мы нашли WeCoders после десятка неудачных проектов с другими web студиями.
-                                И…
-                                Это большая удача, <span>"</span></p>
-                            <div class="testimonial-author text-uppercase">
-                                <span>- Ирина Граф, Cars & Cars company</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
-
+    <? $APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "footer_slider",
+        array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+            "ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+            "AJAX_MODE" => "N",	// Включить режим AJAX
+            "AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+            "AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+            "AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+            "AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
+            "CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+            "CACHE_GROUPS" => "N",	// Учитывать права доступа
+            "CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+            "CACHE_TYPE" => "A",	// Тип кеширования
+            "CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+            "DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+            "DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+            "DISPLAY_DATE" => "N",	// Выводить дату элемента
+            "DISPLAY_NAME" => "N",	// Выводить название элемента
+            "DISPLAY_PICTURE" => "N",	// Выводить изображение для анонса
+            "DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+            "DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+            "FIELD_CODE" => array(	// Поля
+                0 => "NAME",
+                1 => "PREVIEW_TEXT",
+                2 => "DETAIL_TEXT",
+                3 => "",
+            ),
+            "FILTER_NAME" => "",	// Фильтр
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+            "IBLOCK_ID" => "18",	// Код информационного блока
+            "IBLOCK_TYPE" => "footer_slider",	// Тип информационного блока (используется только для проверки)
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+            "INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
+            "MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+            "NEWS_COUNT" => "20",	// Количество новостей на странице
+            "PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+            "PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+            "PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+            "PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+            "PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+            "PAGER_TITLE" => "Новости",	// Название категорий
+            "PARENT_SECTION" => "",	// ID раздела
+            "PARENT_SECTION_CODE" => "",	// Код раздела
+            "PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+            "PROPERTY_CODE" => array(	// Свойства
+                0 => "",
+                1 => "",
+            ),
+            "SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+            "SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+            "SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+            "SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
+            "SET_STATUS_404" => "N",	// Устанавливать статус 404
+            "SET_TITLE" => "N",	// Устанавливать заголовок страницы
+            "SHOW_404" => "N",	// Показ специальной страницы
+            "SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+            "SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+            "SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
+            "SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+            "STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+        ),
+        false
+    ); ?>
 </section>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
