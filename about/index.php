6bf4de432fa3748276cddb91717635cc41033664
Maxim Belyi <btf9458@gmail.com>
2025-12-29 18:27:18 +0500

    добавлен newslist  about_team на страницу о нас

--- about/index.php
+++ about/index.php
@@ -67,76 +67,77 @@ $APPLICATION->SetPageProperty("description", "О нашей компании");
                         ); ?></p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                    <div class="single-team mb-30">
-                        <div class="team-img">
-                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/img/team/team1.jpg" alt="" />
-                            <div class="team-text">
-                                <h4>Привет всем!</h4>
-                                <p>Ни один человек за всю историю не добился ничего великого, не породив этого сначала в
-                                    своем воображении.</p>
-                                <div class="team-icon">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                    <a href="#"><i class="fa fa-linkedin"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="team-name text-center">
-                            <h4>Антон Морозов</h4>
-                            <h5>Креативный директор</h5>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                    <div class="single-team mb-30">
-                        <div class="team-img">
-                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/img/team/team2.jpg" alt="" />
-                            <div class="team-text">
-                                <p>Сосредоточь все силы на достижение желаемого, чтобы у тебя не было времени бояться
-                                    нежелательного.</p>
-                                <div class="team-icon">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                    <a href="#"><i class="fa fa-linkedin"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="team-name text-center">
-                            <h4>Анастасия Любимова</h4>
-                            <h5>Дизайнер</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                    <div class="single-team mb-30">
-                        <div class="team-img">
-                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/img/team/team3.jpg" alt="" />
-                            <div class="team-text">
-                                <h4>Привет!</h4>
-                                <p>Чем дольше вы идете к успеху, тем он становится ближе. Слишком многие бросают дело за
-                                    шаг до победы. Запомните: этот шаг сделают другие.</p>
-                                <div class="team-icon">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                    <a href="#"><i class="fa fa-linkedin"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="team-name text-center">
-                            <h4>Александр Ленский</h4>
-                            <h5>web разработчик</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+            <?$APPLICATION->IncludeComponent("bitrix:news.list", "about_team", Array(
+	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "N",	// Учитывать права доступа
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CHECK_DATES" => "N",	// Показывать только активные на данный момент элементы
+		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+		"DISPLAY_DATE" => "N",	// Выводить дату элемента
+		"DISPLAY_NAME" => "Y",	// Выводить название элемента
+		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"FIELD_CODE" => array(	// Поля
+			0 => "NAME",
+			1 => "PREVIEW_TEXT",
+			2 => "PREVIEW_PICTURE",
+			3 => "DETAIL_TEXT",
+			4 => "",
+		),
+		"FILTER_NAME" => "",	// Фильтр
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+		"IBLOCK_ID" => "19",	// Код информационного блока
+		"IBLOCK_TYPE" => "about",	// Тип информационного блока (используется только для проверки)
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+		"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
+		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+		"NEWS_COUNT" => "20",	// Количество новостей на странице
+		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+		"PAGER_TITLE" => "Новости",	// Название категорий
+		"PARENT_SECTION" => "",	// ID раздела
+		"PARENT_SECTION_CODE" => "",	// Код раздела
+		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+		"PROPERTY_CODE" => array(	// Свойства
+			0 => "social_links",
+			1 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404
+		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+		"SHOW_404" => "N",	// Показ специальной страницы
+		"SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+		"SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
+		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+	),
+	false
+);?>
+            
         </div>
     </div>
 
     <!-- Отзывы -->
-    <section class="testimonial-area pad-90">
+    <!-- <section class="testimonial-area pad-90">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
@@ -181,7 +182,7 @@ $APPLICATION->SetPageProperty("description", "О нашей компании");
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
     <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
 
 </section>
--- test.php
+++ test.php
@@ -4,5 +4,4 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовая");
 ?>
 
-
 <?php require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
